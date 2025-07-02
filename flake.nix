55f0cd3a584a94bf95749a160ea2e20aadcc7f31
osbm <osmanfbayram@gmail.com>
2025-07-02 16:36:51 +0300

    add nix cache

--- flake.nix
+++ flake.nix
@@ -3,6 +3,14 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   outputs = {self, nixpkgs, ... }: let
     forAllSystems = nixpkgs.lib.genAttrs [
       "aarch64-linux"
