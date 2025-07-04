ff1cb804f0757dd6539645314830dde607d72a3b
osbm <osmanfbayram@gmail.com>
2025-07-05 00:24:02 +0300

    add rust

---
+++ devshells/rust.nix
@@ -0,0 +1,27 @@
+{
+  inputs,
+  system,
+  ...
+}: let
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+  };
+in
+  pkgs.mkShell {
+    packages = with pkgs; [
+      # Rust toolchain
+      rustc
+      cargo
+      rustfmt
+      rust-analyzer
+      clippy
+      
+      # Build dependencies
+      pkg-config
+      openssl
+    ];
+    
+    shellHook = ''
+      echo "🦀 Activating Rust development environment"
+    '';
+  }
