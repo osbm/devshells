0771ac72382ed42a1fff49ef380f8441c1a3afa4
osbm <osmanfbayram@gmail.com>
2025-07-04 23:02:39 +0300

    formatting

--- flake.nix
+++ flake.nix
@@ -11,10 +11,7 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
-  outputs = {
-    nixpkgs,
-    ...
-  }: let
+  outputs = {nixpkgs, ...}: let
     forAllSystems = nixpkgs.lib.genAttrs [
       "aarch64-linux"
       "i686-linux"
